c949d1243dc3c8503f9b7b8a703c9a527798b5e9
JJHERNA <jjherna@proteccion.com.co>
2022-11-25 14:41:35 -0500

    feat: change revalidate endpoint structure

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -5,7 +5,7 @@ type Data =
   | { revalidated: boolean }
 
 
-// llame, por ejemplo, api/revalidate?secret=TOKEN&path=/post/<slug> 
+// llame, por ejemplo, api/revalidate?secret=TOKEN, { slug: "post_slug" }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   if (!process.env.REVALIDATE_STATIC_PAGES) {
@@ -17,9 +17,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   }
 
   try {
-    const path = req.query.path ?? '-1'
+    const { path = '-1' } = req.body.parameters as { path: string }
 
-    await res.revalidate(path as string)
+    await res.revalidate(path)
     return res.json({ revalidated: true })
   
   } catch (err) {
